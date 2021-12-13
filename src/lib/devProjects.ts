0f71e9e8d7cb3540811363c35c0b533432fdeda2
Daniel Lopez <daniel@bldrscove.com>
2021-12-13 16:29:48 -0500

    Update Trading Ally project description and screenshots.

--- src/lib/devProjects.ts
+++ src/lib/devProjects.ts
@@ -1,9 +1,9 @@
 import bbsFeatImgLg from '../images/bbs-feat-image-1276px.jpg';
 import bbsFeatImgMd from '../images/bbs-feat-image-768px.jpg';
 import bbsFeatImgSm from '../images/bbs-feat-image-530px.jpg';
-import tradingJournalFeatImgLg from '../images/trading-journal-feat-image-1276px.jpg';
-import tradingJournalFeatImgMd from '../images/trading-journal-feat-image-768px.jpg';
-import tradingJournalFeatImgSm from '../images/trading-journal-feat-image-530px.jpg';
+import tradingAllyFeatImgLg from '../images/trading-ally-feat-image-1276px.jpg';
+import tradingAllyFeatImgMd from '../images/trading-ally-feat-image-768px.jpg';
+import tradingAllyFeatImgSm from '../images/trading-ally-feat-image-530px.jpg';
 
 const webDevProjects: {
 		title: string;
@@ -33,25 +33,25 @@ const webDevProjects: {
 				'Build Beautiful Spaces is a small business that specializes in restoration and interior renovation. \n \n Lacking an online presence, the project consisted of developing a user friendly website that would not sacrifice on speed due to its image-heavy content. For this reason, I decided on Gatsby’s SSG build, and implemented caching and lazy loading. \n \n Integrating pagination for the main work page also made sense from a performance and UX standpoint. The file upload and email integrations leverage 3rd party APIs, which makes the contact user experience easy and help prevent bot submissions.',
 			excerpt: 'The BBS website build project consisted of developing a clean and fast portfolio that would not sacrifice speed due to its image-heavy content.',
 			techStack: ['Gatsby', 'Slider JS', 'Framer Motion', 'Cloudinary', 'SendGrid'],
-			gitHubLink: 'https://github.com/tgdpez/BBS-Website',
+			gitHubLink: 'https://github.com/daniel-bldrscove/BBS-Website',
 			projectLink: 'https://buildbeautifulspaces.com'
 		},
 		{
-			title: 'Delta North Trading Journal',
-			subhead: 'CRUD Tracking Journal',
+			title: 'Trading Ally',
+			subhead: 'Trade tracking C.R.U.D. application',
 			coverImage: {
-				lg: tradingJournalFeatImgLg,
-				md: tradingJournalFeatImgMd,
-				sm: tradingJournalFeatImgSm,
+				lg: tradingAllyFeatImgLg,
+				md: tradingAllyFeatImgMd,
+				sm: tradingAllyFeatImgSm,
 			},
-			coverImageAlt: 'Delta North trading journal app ui screenshot',
+			coverImageAlt: 'Trading Ally journal app ui screenshot',
 			description:
-				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n \n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				'Trading Ally helps keep trading goals front and center, highlighting user performance in a way that\'s flexible and easy to understand. \n\n Although there are many apps that help track market data, the user interface and data can still be complicated to understand, especially for the new investors. This is where Trading Ally helps distill noisy data into a simple and clear UI. \n\n Although still in development, Trading Ally seeks to help users answer three simple questions. \n\n - "What was my past trading performance?" \n - "What is my current trading performance?" \n - "Where do I want my future trading performance to be?" \n\n Trading Ally empowers it\'s users by giving them control over their own data and provides suggestions that can help investors achieve their goals.',
 			excerpt:
-				'This is a dev card descrition with a read more link at the end triggering a modal pop-up',
-			techStack: ['React', 'Node JS', 'TD Ameritrade API', 'React Query', 'Material UI'],
-			gitHubLink: 'https://github.com/tgdpez/TradingJournal',
-			projectLink: 'https://deltanorthtradingjournal.com'
+				'Trading Ally helps keep trading goals front and center, highlighting user performance in a way that\'s flexible and easy to understand.',
+			techStack: ['React JS', 'Typescript', 'Chakra UI', 'Fauna DB', 'React Table', 'React Query'],
+			gitHubLink: 'https://github.com/daniel-bldrscove/trading-ally',
+			projectLink: 'https://tradingally.netlify.app/'
 		}
 	];
 
