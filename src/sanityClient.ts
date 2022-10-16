8a453dd7473061d389f6d401db224553b5252ffd
Daniel Lopez <daniel@bldrscove.com>
2022-10-16 15:54:07 -0400

    Updated Sanity Studio to latest account.

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 
 // allows connection between svelete and sanity
 const client = sanityClient({
-	projectId: 'fv0ic7lw',
+	projectId: 'wydp43aa',
 	dataset: 'production',
 	apiVersion: '2021-09-01', // use current UTC date - see "specifying API version"!
 	useCdn: true,
