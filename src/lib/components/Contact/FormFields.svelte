a4a14ebbb476566d9a076eb7a99bcc7eeea86978
Daniel Lopez <daniel@bldrscove.com>
2024-03-30 20:29:26 -0400

    Correct global assignment to handleSubmit.

--- src/lib/components/Contact/FormFields.svelte
+++ src/lib/components/Contact/FormFields.svelte
@@ -81,7 +81,7 @@
 
 	onMount(() => {
 		// initialize handleSubmit method to window in order to run recaptcha on submit
-		window.handleSubmit = () => handleSubmit;
+		window.handleSubmit = handleSubmit;
 	});
 </script>
 
